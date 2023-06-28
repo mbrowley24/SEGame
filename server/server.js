ae3f97053d20f7b62269a3938e811321b147428f
mbrowley24 <74025082+mbrowley24@users.noreply.github.com>
2023-06-28 11:33:41 -0600

    game working

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://urchin-app-voqyy.ondigitalocean.app",
+        origin: "https://urchin-app-voqyy.ondigitalocean.app/",
         methods: ["GET", "POST"],
         credentials: false
     }
