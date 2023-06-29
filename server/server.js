bdf6fb1aae7c1ff7ff8cb4593268fb7fbd026643
mbrowley24 <74025082+mbrowley24@users.noreply.github.com>
2023-06-28 21:16:27 -0600

    game working

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const {Server} = require("socket.io");
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors({credentials: true, origin: 'http://localhost'}));
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(cookieParser());
 
 require('./config/config');
@@ -29,7 +29,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost",
+        origin: "http://localhost:3000",
         methods: ["GET", "POST"],
     }
 })
